ae25c578dbd81ee4646b7c4e181e21883ee959d6
Alex McArdle <alex@mcardle.io>
2026-07-24 23:20:43 -0500

    Add native Metal renderer

--- apps/cwr/Game/GameApplication.cpp
+++ apps/cwr/Game/GameApplication.cpp
@@ -1479,6 +1479,9 @@ void GameApplication::RegisterGraphicsBackends()
 {
     RegisterDummyGraphicsBackend();
     RegisterGL33GraphicsBackend();
+#ifdef __APPLE__
+    RegisterMetalGraphicsBackend();
+#endif
 }
 
 bool GameApplication::InitializeInput()
